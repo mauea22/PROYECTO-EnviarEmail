3d86687acae5445c2d05b247d12c4c31aa19b944
mauea22 <mauroallena@gmail.com>
2022-05-18 23:36:40 -0300

    validaciones email

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //variables
 const btnEnviar = document.querySelector('#enviar');
+const formulario = document.querySelector('#enviar-mail');
 
 //variables para campos
 
@@ -12,7 +13,8 @@ function eventListener(){
     // cuando la app arranca
     document.addEventListener('DOMContentLoaded', iniciarApp);
 
-    // campos del formulario
+    /* campos del formulario
+    blur se dispara cuando salgo del input */
     email.addEventListener('blur', validaFormulario);
     asunto.addEventListener('blur', validaFormulario);
     mensaje.addEventListener('blur', validaFormulario);
@@ -31,8 +33,42 @@ function iniciarApp() {
 function validaFormulario(e) {
     
     if (e.target.value.length > 0) {
-        console.log("si hay algo");
+        // elimina los errores....
+        const error = document.querySelector('p.error');
+        error.remove();
+        // intercambio de clases
+        e.target.classList.remove('border', 'border-red-500');
+        e.target.classList.add('border', 'border-green-500');
     } else {
-        e.target.classList.add('error', 'border-red-500')
+        e.target.classList.remove('border', 'border-green-500');
+        e.target.classList.add('border', 'border-red-500');
+        mostrarError('Todos los campos son obligatorios');
     }
+
+    if(e.target.type === 'email') {
+        const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if( er.test( e.target.value)) {
+            e.target.classList.remove('border', 'border-red-500');
+            e.target.classList.add('border', 'border-green-500');
+        } else {
+            e.target.classList.remove('border', 'border-green-500');
+            e.target.classList.add('border', 'border-red-500');
+            mostrarError('Email no  válido');
+        }
+    }
+}
+
+function mostrarError(mensaje) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = mensaje;
+    mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
+
+    //mostrar solo un mensaje de error
+    //usamos  querySelectorAll para tener acceso a una colección y poder verificar la propiedad length
+    const errores = document.querySelectorAll('.error');
+    if (errores.length === 0) {
+        formulario.appendChild(mensajeError);
+    }
+
 }
